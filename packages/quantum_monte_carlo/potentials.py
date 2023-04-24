8c42b9c4f2b2f21bbc574e9751b1d14dc7bbb1fa
anacmolina <anac.molina@udea.edu.co>
2023-04-24 16:48:37 -0500

    package cool, better do a simple and efficient code

--- packages/quantum_monte_carlo/potentials.py
+++ packages/quantum_monte_carlo/potentials.py
@@ -3,12 +3,16 @@
 import numpy as np
 
 ### Define the harmonic oscillator potential
-def quantum_harmonic_oscillator(x):
-    """Quamtum armonic oscillator potential
-    
-    Args:
-        x (float): position of the particle
-    Returns:
-        float: value of the potential at x"""
-    return 0.5*x**2
+class Potentials:
+    """Class for defining potentials"""
+
+    def harmonic_oscillator(self, x):
+        """Quamtum armonic oscillator potential
+        
+        Args:
+            x (float): position of the particle
+        Returns:
+            float: value of the potential at x"""
+        return 0.5*x**2
+
 
--- packages/quantum_monte_carlo/probabilities.py
+++ packages/quantum_monte_carlo/probabilities.py
@@ -1,8 +1,14 @@
 import numpy as np
 
-class Probabilities:
-    def __init__(self) -> None:
-        pass
-
-    def 
+# Classic particle probability
+def classical_particle_probability(x, V, T):
+    """Classical particle probability.
+    Args:
+        x (float): position of the particle
+        V (function): potential energy function
+        T (float): temperature
+    Returns:
+        float: probability of the particle being at x"""
+    beta = 1/T
+    return np.exp(-beta*V(x))
         
